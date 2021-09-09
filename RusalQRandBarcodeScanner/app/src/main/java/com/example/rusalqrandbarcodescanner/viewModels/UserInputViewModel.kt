f5489359193b08b51d0a28e27ec4d479e7863660
kyleConrad22 <89704118+kyleConrad22@users.noreply.github.com>
2021-09-08 19:10:47 -0500

    Refactor: Remove Redundant Method
    Removed update() method from UserInputViewModel module because it is now redundant.

--- RusalQRandBarcodeScanner/app/src/main/java/com/example/rusalqrandbarcodescanner/viewModels/UserInputViewModel.kt
+++ RusalQRandBarcodeScanner/app/src/main/java/com/example/rusalqrandbarcodescanner/viewModels/UserInputViewModel.kt
@@ -14,28 +14,6 @@ class UserInputViewModel(): ViewModel() {
     var heat: MutableLiveData<String> = MutableLiveData("")
     var quantity: MutableLiveData<String> = MutableLiveData("")
 
-    fun update(
-        loader: String,
-        order: String,
-        load: String,
-        bundles: String,
-        bl: String,
-        vessel: String,
-        checker: String,
-        heat: String,
-        quantity: String
-    ) {
-        this.loader.value = loader
-        this.order.value = order
-        this.load.value = load
-        this.bundles.value = bundles
-        this.bl.value = bl
-        this.vessel.value = vessel
-        this.checker.value = checker
-        this.heat.value = heat
-        this.quantity.value = quantity
-    }
-
     fun updateHeat(heat: String){
         this.heat.value = heat
     }
