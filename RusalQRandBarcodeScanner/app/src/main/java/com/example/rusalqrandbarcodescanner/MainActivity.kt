f1257956a42f2b3d5a7dee87a8594d6e12a701d3
kyleConrad22 <89704118+kyleConrad22@users.noreply.github.com>
2021-09-12 13:48:30 -0500

    Refactor: Move Bundle Info Screen Logic
    Moved logic for bundle info screen to separate module to increase maintainability of code.

--- RusalQRandBarcodeScanner/app/src/main/java/com/example/rusalqrandbarcodescanner/MainActivity.kt
+++ RusalQRandBarcodeScanner/app/src/main/java/com/example/rusalqrandbarcodescanner/MainActivity.kt
@@ -113,89 +113,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    @Composable
-    fun BundleInfo(barcode: String?, navController: NavHostController) {
-        var scannedCode by remember { mutableStateOf(scannedCodeViewModel.findByBarcode(barcode!!).value) }
-        val codeObserver = Observer<ScannedCode?> { it ->
-            scannedCode = it
-        }
-        var count by remember { mutableStateOf(scannedCodeViewModel.count.value) }
-        val countObserver = Observer<Int> { it ->
-            count = it
-        }
-        var isLoad by remember { mutableStateOf(userInputViewModel.isLoad().value) }
-        val isLoadObserver = Observer<Boolean> { it->
-            isLoad = it
-        }
-        userInputViewModel.isLoad().observe(this@MainActivity, isLoadObserver)
-        scannedCodeViewModel.count.observe(this@MainActivity, countObserver)
-        scannedCodeViewModel.findByBarcode(barcode!!).observe(this@MainActivity, codeObserver)
-
-        var openDialog by remember { mutableStateOf(false) }
-
-        Column(modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.SpaceEvenly,
-            horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(text="Heat Number: ${scannedCode?.heatNum}")
-            Text(text="BL Number: ${scannedCode?.bl}")
-            Text(text="Quantity: ${scannedCode?.quantity}")
-            Text(text="Net Weight Kg: ${scannedCode?.netWgtKg}")
-            Text(text="Gross Weight Kg: ${scannedCode?.grossWgtKg}")
-            Text(text="Barcode: $barcode")
-            Row(modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceEvenly) {
-                Button(onClick = {
-                    navController.navigate(navController.previousBackStackEntry?.destination?.route!!)
-                }) {
-                    Text(text = "Dismiss", modifier = Modifier.padding(16.dp))
-                }
-                Button(onClick = {
-                    openDialog = true
-                }) {
-                    Text(text = "Remove From Shipment", modifier = Modifier.padding(16.dp))
-                }
-                if (openDialog) {
-                    AlertDialog(onDismissRequest = {
-                            openDialog = false
-                        }, title = {
-                            Text(text = "Removal Confirmation", modifier = Modifier.padding(16.dp))
-                        }, text = {
-                            Text(text = "Are you sure you would like to remove this bundle from the load?",
-                                modifier = Modifier.padding(16.dp))
-                        }, buttons = {
-                            Row(modifier = Modifier.fillMaxWidth(),
-                                verticalAlignment = Alignment.CenterVertically,
-                                horizontalArrangement = Arrangement.SpaceEvenly) {
-                                Button(onClick = {
-                                    openDialog = false
-                                }) {
-                                    Text(text = "Deny Removal", modifier = Modifier.padding(16.dp))
-                                }
-                                Button(onClick = {
-                                    scannedCodeViewModel.delete(scannedCode!!)
-                                    openDialog = false
-                                    if (count != null && count!! > 1) {
-                                        navController.navigateUp()
-                                    } else {
-                                        if (isLoad!!){
-                                            navController.popBackStack("loadOptionsPage", inclusive = false)
-                                        } else {
-                                            navController.popBackStack("receptionOptionsPage", inclusive = false)
-                                        }
-                                    }
-
-                                }) {
-                                    Text(text = "Confirm Removal", modifier = Modifier.padding(16.dp))
-                                }
-                            }
-                        }
-                    )
-                }
-            }
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -795,7 +712,7 @@ class MainActivity : ComponentActivity() {
                     DuplicateBundleScreen(navController = navController, scanTime = backStackEntry.arguments?.getString("scanTime"), scannedCodeViewModel =  scannedCodeViewModel, userInputViewModel = userInputViewModel)
                 }
                 composable(Screen.BundleInfoScreen.title + "/{barcode}") { backStackEntry ->
-                    BundleInfoScreen(navController = navController, barcode = backStackEntry.arguments?.getString("barcode"))
+                    BundleInfoScreen(navController = navController, barcode = backStackEntry.arguments?.getString("barcode"), scannedCodeViewModel = scannedCodeViewModel, userInputViewModel = userInputViewModel)
                 }
                 composable(Screen.ConfirmationScreen.title) { ConfirmationScreen(navController = navController, userInputViewModel = userInputViewModel) }
                 composable(Screen.ReceptionReviewScreen.title) { ReceptionReviewScreen(navController = navController, scannedCodeViewModel = scannedCodeViewModel) }
--- RusalQRandBarcodeScanner/app/src/main/java/com/example/rusalqrandbarcodescanner/screens/BundleInfoScreen.kt
+++ RusalQRandBarcodeScanner/app/src/main/java/com/example/rusalqrandbarcodescanner/screens/BundleInfoScreen.kt
@@ -1,8 +1,103 @@
 package com.example.rusalqrandbarcodescanner.screens
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Observer
 import androidx.navigation.NavHostController
+import com.example.rusalqrandbarcodescanner.Screen
+import com.example.rusalqrandbarcodescanner.database.ScannedCode
+import com.example.rusalqrandbarcodescanner.viewModels.ScannedCodeViewModel
+import com.example.rusalqrandbarcodescanner.viewModels.UserInputViewModel
 
 @Composable
-fun BundleInfoScreen(navController: NavHostController, barcode: String?) {
+fun BundleInfoScreen(navController: NavHostController, barcode: String?, scannedCodeViewModel: ScannedCodeViewModel, userInputViewModel: UserInputViewModel) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    var scannedCode by remember { mutableStateOf(scannedCodeViewModel.findByBarcode(barcode!!).value) }
+    val codeObserver = Observer<ScannedCode?> { it ->
+        scannedCode = it
+    }
+    var count by remember { mutableStateOf(scannedCodeViewModel.count.value) }
+    val countObserver = Observer<Int> { it ->
+        count = it
+    }
+    var isLoad by remember { mutableStateOf(userInputViewModel.isLoad().value) }
+    val isLoadObserver = Observer<Boolean> { it->
+        isLoad = it
+    }
+
+    userInputViewModel.isLoad().observe(lifecycleOwner, isLoadObserver)
+    scannedCodeViewModel.count.observe(lifecycleOwner, countObserver)
+    scannedCodeViewModel.findByBarcode(barcode!!).observe(lifecycleOwner, codeObserver)
+
+    var openDialog by remember { mutableStateOf(false) }
+
+    Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text="Heat Number: ${scannedCode?.heatNum}")
+        Text(text="BL Number: ${scannedCode?.bl}")
+        Text(text="Quantity: ${scannedCode?.quantity}")
+        Text(text="Net Weight Kg: ${scannedCode?.netWgtKg}")
+        Text(text="Gross Weight Kg: ${scannedCode?.grossWgtKg}")
+        Text(text="Barcode: $barcode")
+        Row(modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+            Button(onClick = {
+                navController.popBackStack()
+            }) {
+                Text(text = "Dismiss", modifier = Modifier.padding(16.dp))
+            }
+            Button(onClick = {
+                openDialog = true
+            }) {
+                Text(text = "Remove From Shipment", modifier = Modifier.padding(16.dp))
+            }
+            if (openDialog) {
+                AlertDialog(onDismissRequest = {
+                    openDialog = false
+                }, title = {
+                    Text(text = "Removal Confirmation", modifier = Modifier.padding(16.dp))
+                }, text = {
+                    Text(text = "Are you sure you would like to remove this bundle from the load?",
+                        modifier = Modifier.padding(16.dp))
+                }, buttons = {
+                    Row(modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceEvenly) {
+                        Button(onClick = {
+                            openDialog = false
+                        }) {
+                            Text(text = "Deny Removal", modifier = Modifier.padding(16.dp))
+                        }
+                        Button(onClick = {
+                            scannedCodeViewModel.delete(scannedCode!!)
+                            openDialog = false
+                            if (count != null && count!! > 1) {
+                                navController.navigateUp()
+                            } else {
+                                if (isLoad!!){
+                                    navController.popBackStack(Screen.LoadOptionsScreen.title, inclusive = false)
+                                } else {
+                                    navController.popBackStack(Screen.ReceptionOptionsScreen.title, inclusive = false)
+                                }
+                            }
+
+                        }) {
+                            Text(text = "Confirm Removal", modifier = Modifier.padding(16.dp))
+                        }
+                    }
+                }
+                )
+            }
+        }
+    }
 }
