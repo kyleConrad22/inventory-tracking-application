b0618d1a1e48ae4f5d75459882b5aecb191b6023
kyleConrad22 <89704118+kyleConrad22@users.noreply.github.com>
2021-09-25 11:47:58 -0500

    Add Guiding Function Skeletons to ScannerViewModel
    Added Guiding Function Skeletons to ScannerViewModel to assist in future movement of logic

--- RusalQRandBarcodeScanner/app/src/main/java/com/example/rusalqrandbarcodescanner/viewModels/ScannerViewModel.kt
+++ RusalQRandBarcodeScanner/app/src/main/java/com/example/rusalqrandbarcodescanner/viewModels/ScannerViewModel.kt
@@ -1,18 +1,38 @@
 package com.example.rusalqrandbarcodescanner.viewModels
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.example.rusalqrandbarcodescanner.repositories.CodeRepository
-import com.example.rusalqrandbarcodescanner.repositories.CurrentInventoryRepository
 import com.example.rusalqrandbarcodescanner.repositories.UserInputRepository
+import kotlinx.coroutines.*
 import java.lang.IllegalArgumentException
 
-class ScannerViewModel(private val userRepo : UserInputRepository, private val codeRepo : CodeRepository, private val invRepo : CurrentInventoryRepository) : ViewModel() {
-    class ScannerViewModelFactory(private val userRepo : UserInputRepository, private val codeRepo : CodeRepository, private val invRepo : CurrentInventoryRepository) : ViewModelProvider.Factory {
+@DelicateCoroutinesApi
+class ScannerViewModel(private val userRepo : UserInputRepository) : ViewModel() {
+    val loading = mutableStateOf(false)
+    val heat = mutableStateOf("")
+
+    /* TODO - Move Scanning Logic To ViewModel */
+
+    /* TODO - Update UserRepository with scanned heat number */
+    fun updateHeat() {
+        loading.value = true
+        GlobalScope.launch(Dispatchers.Main) {
+            val value = GlobalScope.async {
+                withContext(Dispatchers.Main) {
+                    userRepo.updateHeat(heat.value)
+                }
+            }
+            println(value.await())
+            loading.value = false
+        }
+    }
+
+    class ScannerViewModelFactory(private val userRepo : UserInputRepository) : ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
             if (modelClass.isAssignableFrom(ScannerViewModel::class.java)) {
                 @Suppress("UNCHECKED_CAST")
-                return ScannerViewModel(userRepo, codeRepo, invRepo) as T
+                return ScannerViewModel(userRepo) as T
             }
             throw IllegalArgumentException("Unknown ViewModel class")
         }
