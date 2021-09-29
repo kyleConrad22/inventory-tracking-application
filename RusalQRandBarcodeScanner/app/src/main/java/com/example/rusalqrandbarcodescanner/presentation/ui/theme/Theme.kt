e60c7f287b1f507b384ec1ea70d0f6e2198541db
kyleConrad22 <89704118+kyleConrad22@users.noreply.github.com>
2021-09-28 19:38:23 -0500

    Feat: Change StatusBar Color and Icon Colors to Match Application Theme

--- RusalQRandBarcodeScanner/app/src/main/java/com/example/rusalqrandbarcodescanner/presentation/ui/theme/Theme.kt
+++ RusalQRandBarcodeScanner/app/src/main/java/com/example/rusalqrandbarcodescanner/presentation/ui/theme/Theme.kt
@@ -5,6 +5,9 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
         primary = Purple200,
@@ -29,15 +32,18 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun RusalQRAndBarcodeScannerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
-    /*
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-     */
+fun RusalQRAndBarcodeScannerTheme(content: @Composable() () -> Unit) {
+    val systemUiController = rememberSystemUiController()
+    val useDarkIcons = MaterialTheme.colors.isLight
+
     val colors = LightColorPalette
+    SideEffect {
+        systemUiController.setSystemBarsColor(
+            color = Color.Transparent,
+            darkIcons = false
+        )
+    }
+
 
     MaterialTheme(
             colors = colors,
