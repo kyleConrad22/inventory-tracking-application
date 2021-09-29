51ce017baa69f5b7ed6b0aac1e8e9788979ee524
kyleConrad22 <89704118+kyleConrad22@users.noreply.github.com>
2021-09-29 07:56:39 -0500

    Refactor: Remove Redundant Fields From ScannedInfo Singleton Object
    Fixed issue where ScannedInfo heat number would not be reset after scanning completed.

--- RusalQRandBarcodeScanner/app/src/main/java/com/example/rusalqrandbarcodescanner/util/ScannedInfo.kt
+++ RusalQRandBarcodeScanner/app/src/main/java/com/example/rusalqrandbarcodescanner/util/ScannedInfo.kt
@@ -6,84 +6,12 @@ import java.time.format.DateTimeFormatter
 
 object ScannedInfo {
     var heatNum: String = ""
-    var barCode: String = ""
-    var packageNum: String = ""
-    var netWgtKg: String = ""
-    var grossWgtKg: String = ""
-    var netWgtLbs: String = ""
-    var grossWgtLbs: String = ""
-    var scanTime: String = ""
-    var blNum: String = ""
-    var quantity: String = ""
 
     fun setValues(rawValue: String) {
         if (rawValue.contains("_")) {
             val elements = rawValue.split("_").toTypedArray()
-            barCode = elements[0]
             heatNum = elements[1].replace("-", "")
-            netWgtKg = elements[5].split("/")[0]
-            grossWgtKg = elements[5].split("/")[1]
-            netWgtLbs = elements[7].split("/")[0]
-            grossWgtLbs = elements[7].split("/")[1]
-            packageNum = elements[9]
-            quantity = try {
-                elements[11]
-            } catch (e: ArrayIndexOutOfBoundsException) {
-                "5"
-            }
-            setTime()
         }
     }
 
-    private fun setTime(){
-        val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("MM_dd_yyyy HH:mm:ss")
-        val timeNow: LocalDateTime = LocalDateTime.now()
-        scanTime = formatter.format(timeNow)
-    }
-
-    fun getValues(currentInventoryLineItem: CurrentInventoryLineItem){
-        barCode = currentInventoryLineItem.barcode
-        blNum = currentInventoryLineItem.blNum.toString()
-        heatNum = currentInventoryLineItem.heatNum.toString()
-        netWgtKg = currentInventoryLineItem.netWeightKg.toString()
-        grossWgtKg = currentInventoryLineItem.grossWeightKg.toString()
-        packageNum = currentInventoryLineItem.packageNum.toString()
-        quantity = currentInventoryLineItem.quantity.toString()
-        setTime()
-    }
-
-    fun setValues(
-        heatNum : String,
-        blNum : String,
-        barcode : String = "",
-        packageNum : String = "",
-        netWeightKg : String = "",
-        grossWeightKg : String = "",
-        netWeightLbs : String = "",
-        grossWeightLbs : String = "",
-        quantity : String = "",
-    ) {
-        ScannedInfo.heatNum = heatNum
-        barCode = barcode
-        grossWgtKg = grossWeightKg
-        netWgtKg = netWeightKg
-        ScannedInfo.packageNum = packageNum
-        netWgtLbs = netWeightLbs
-        grossWgtLbs = grossWeightLbs
-        ScannedInfo.quantity = quantity
-        ScannedInfo.blNum = blNum
-    }
-
-    fun clearValues(){
-        heatNum = ""
-        barCode = ""
-        netWgtKg = ""
-        grossWgtKg = ""
-        netWgtLbs = ""
-        grossWgtLbs = ""
-        packageNum = ""
-        scanTime = ""
-        blNum = ""
-        quantity = ""
-    }
 }
--- RusalQRandBarcodeScanner/app/src/main/java/com/example/rusalqrandbarcodescanner/viewmodels/ScannerViewModel.kt
+++ RusalQRandBarcodeScanner/app/src/main/java/com/example/rusalqrandbarcodescanner/viewmodels/ScannerViewModel.kt
@@ -16,12 +16,16 @@ class ScannerViewModel(private val userRepo : UserInputRepository) : ViewModel()
 
     /* TODO - Move Scanning Logic To ViewModel */
 
+    init {
+    }
+
     fun updateHeat() {
         loading.value = true
         GlobalScope.launch(Dispatchers.Main) {
             val value = GlobalScope.async {
                 withContext(Dispatchers.Main) {
                     userRepo.updateHeat(ScannedInfo.heatNum)
+                    ScannedInfo.heatNum = ""
                 }
             }
             println(value.await())
