687353fddc0899b3494d6697885b1d15f66a5c57
kyleConrad22 <89704118+kyleConrad22@users.noreply.github.com>
2021-10-02 21:23:00 -0500

    Add GUI logic for Downloads Function of Rusal Application
    Added Basic GUI of downloads function for Rusal Application.

--- Nasco-Database-Web-App/src/main/js/components/apps/rusal_app/rusal_downloads.jsx
+++ Nasco-Database-Web-App/src/main/js/components/apps/rusal_app/rusal_downloads.jsx
@@ -1,8 +1,31 @@
 import React from "react";
+import ToBeImplemented from "../../util/to_be_implemented";
 
 export default function RusalDownloads() {
 
-    return (
+    function DownloadDatabase() {
+        return(
+            <button id='download-database' type='button'>Download Full Database</button>
+        );
+    }
 
+    function DownloadByOrderAndLoad() {
+        return (
+            <form>
+                <input id='work-order' name='workOrder' type='text' placeholder='Enter Work Order:'/>
+                <input id='load-number' name='loadNum' type='text' placeholder='Enter Load Number:'/>
+                <button id='download-order-and-load' type='button'>Download By Order and Load</button>
+            </form>
+        );
+    }
+
+    return (
+        <div id='downloads'>
+            <h3>Download Links</h3>
+            <ToBeImplemented />
+            <DownloadDatabase />
+            &nbsp;&nbsp;&nbsp;
+            <DownloadByOrderAndLoad />
+        </div>
     );
 }
