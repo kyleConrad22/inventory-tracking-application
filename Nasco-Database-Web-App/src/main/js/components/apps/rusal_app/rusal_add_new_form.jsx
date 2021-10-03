5e25da95222055e3ea1e369f363694c2cfc29b80
kyleConrad22 <89704118+kyleConrad22@users.noreply.github.com>
2021-10-02 21:10:52 -0500

    Update GUI of Rusal Add New Form
    Added a header and spacing to items in Add New Item Form for Rusal Application

--- Nasco-Database-Web-App/src/main/js/components/apps/rusal_app/rusal_add_new_form.jsx
+++ Nasco-Database-Web-App/src/main/js/components/apps/rusal_app/rusal_add_new_form.jsx
@@ -2,19 +2,32 @@ import React from "react";
 
 const RusalAddNewForm = ({ handleSubmit }) => {
     return (
-        <form onSubmit={ handleSubmit }>
-            <input id="heatNum" name="heatNum" type="text" placeholder="Enter Heat Number"/>
-            <input id="packageNum" name="packageNum" type="text" placeholder="Enter Package Number"/>
-            <input id="grossWeightKg" name="grossWeightKg" type="text" placeholder="Enter Gross Weight Kg"/>
-            <input id="netWeightKg" name="netWeightKg" type="text" placeholder="Enter Net Weight"/>
-            <input id="quantity" name="quantity" type="text" placeholder="Enter Quantity"/>
-            <input id="dimension" name="dimension" type="text" placeholder="Enter Dimension"/>
-            <input id="grade" name="grade" type="text" placeholder="Enter Grade"/>
-            <input id="certificateNum" name="certificateNum" type="text" placeholder="Enter Certificate Number"/>
-            <input id="blNum" name="blNum" type="text" placeholder="Enter BL Number"/>
-            <input id="barcode" name="barcode" type="text" placeholder = "Enter Barcode"/>
-            <button type="submit">Add New Item</button>
-        </form>
+        <div>
+            <h3>Add New Rusal Item</h3>
+            <form onSubmit={ handleSubmit }>
+                <input id="heatNum" name="heatNum" type="text" placeholder="Enter Heat Number"/>
+                &nbsp;&nbsp;&nbsp;
+                <input id="packageNum" name="packageNum" type="text" placeholder="Enter Package Number"/>
+                &nbsp;&nbsp;&nbsp;
+                <input id="grossWeightKg" name="grossWeightKg" type="text" placeholder="Enter Gross Weight Kg"/>
+                &nbsp;&nbsp;&nbsp;
+                <input id="netWeightKg" name="netWeightKg" type="text" placeholder="Enter Net Weight"/>
+                &nbsp;&nbsp;&nbsp;
+                <input id="quantity" name="quantity" type="text" placeholder="Enter Quantity"/>
+                &nbsp;&nbsp;&nbsp;
+                <input id="dimension" name="dimension" type="text" placeholder="Enter Dimension"/>
+                &nbsp;&nbsp;&nbsp;
+                <input id="grade" name="grade" type="text" placeholder="Enter Grade"/>
+                &nbsp;&nbsp;&nbsp;
+                <input id="certificateNum" name="certificateNum" type="text" placeholder="Enter Certificate Number"/>
+                &nbsp;&nbsp;&nbsp;
+                <input id="blNum" name="blNum" type="text" placeholder="Enter BL Number"/>
+                &nbsp;&nbsp;&nbsp;
+                <input id="barcode" name="barcode" type="text" placeholder = "Enter Barcode"/>
+                &nbsp;&nbsp;&nbsp;
+                <button type="submit">Add New Item</button>
+            </form>
+        </div>
     );
 }
 
