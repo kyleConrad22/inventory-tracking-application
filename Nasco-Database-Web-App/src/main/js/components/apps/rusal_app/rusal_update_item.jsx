a577fdbac4a7536603e3fd8cfca4ff10262ac706
kyleConrad22 <89704118+kyleConrad22@users.noreply.github.com>
2021-10-02 21:04:51 -0500

    Add Skeleton GUI for RusalUpdateItem Function
    Added a skeleton GUI for the update item function of Rusal application.

--- Nasco-Database-Web-App/src/main/js/components/apps/rusal_app/rusal_update_item.jsx
+++ Nasco-Database-Web-App/src/main/js/components/apps/rusal_app/rusal_update_item.jsx
@@ -1,8 +1,35 @@
 import React from "react";
+import ToBeImplemented from "../../util/to_be_implemented";
 
 export default function RusalUpdateItem() {
 
+    function UpdateFields() {
+        return (
+            <form>
+                <input id="heatNum" name='heatNum' type='text' placeholder='Enter Bundle Heat Number:' />
+                &nbsp;&nbsp;&nbsp;
+                <input id='workOrder' name='workOrder' type='text' placeholder='Enter Work Order:' />
+                &nbsp;&nbsp;&nbsp;
+                <input id='loadNum' name='loadNum' type='text' placeholder='Enter Load Number:' />
+                &nbsp;&nbsp;&nbsp;
+                <input id='loader' name='loader' type='text' placeholder='Enter Loader Name:' />
+                &nbsp;&nbsp;&nbsp;
+                <input id='loadTime' name='loadTime' type='text' placeholder='Enter Load Time:' />
+                &nbsp;&nbsp;&nbsp;
+                <input id='vessel' name='vessel' type='text' placeholder='Enter Vessel Name:' />
+                &nbsp;&nbsp;&nbsp;
+                <input id='checker' name='checker' type='text' placeholder='Enter Checker Name:' />
+                &nbsp;&nbsp;&nbsp;
+                <button id='submit' type='submit'>Submit</button>
+            </form>
+        );
+    }
+
     return (
-        
+        <div id='update-rusal'>
+            <h3>Update Rusal Item</h3>
+            <ToBeImplemented />
+            <UpdateFields />
+        </div>
     );
 }
