b3fc1d1d6d945febc85d8609c949a097a6195ae7
kyleConrad22 <89704118+kyleConrad22@users.noreply.github.com>
2021-10-16 15:45:33 -0500

    Refactor: Moved Screen Specific ViewModels to Sub-Package of ViewModels

--- RusalQRandBarcodeScanner/app/src/main/java/com/example/rusalqrandbarcodescanner/viewmodels/screen_viewmodels/ScannerViewModel.kt
+++ RusalQRandBarcodeScanner/app/src/main/java/com/example/rusalqrandbarcodescanner/viewmodels/screen_viewmodels/ScannerViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.rusalqrandbarcodescanner.viewmodels
+package com.example.rusalqrandbarcodescanner.viewmodels.screen_viewmodels
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
