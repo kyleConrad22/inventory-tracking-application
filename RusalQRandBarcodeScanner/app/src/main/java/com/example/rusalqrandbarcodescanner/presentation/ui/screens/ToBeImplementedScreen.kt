91781942ac11ae19d00144a62910f1be6289303f
kyleConrad22 <89704118+kyleConrad22@users.noreply.github.com>
2021-10-18 09:44:34 -0500

    Update Formatting of To Be Implemented Screen

--- RusalQRandBarcodeScanner/app/src/main/java/com/example/rusalqrandbarcodescanner/presentation/ui/screens/ToBeImplementedScreen.kt
+++ RusalQRandBarcodeScanner/app/src/main/java/com/example/rusalqrandbarcodescanner/presentation/ui/screens/ToBeImplementedScreen.kt
@@ -2,6 +2,7 @@ package com.example.rusalqrandbarcodescanner.presentation.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
@@ -11,13 +12,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
 @Composable
 fun ToBeImplementedScreen(navController: NavHostController) {
     Scaffold(topBar = { TopAppBar(title = { Text(text= "Not Yet Implemented", textAlign = TextAlign.Center) }) }) {
-        Column(modifier = Modifier.padding(16.dp),
+        Column(modifier = Modifier.padding(16.dp).fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.SpaceEvenly) {
             Text(text = "This feature has yet to be implemented!")
