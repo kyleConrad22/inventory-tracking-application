d69320a25cfe79ba288e89e426a91334c642ad2f
kyleConrad22 <89704118+kyleConrad22@users.noreply.github.com>
2021-10-21 11:41:53 -0500

    Update Network Request Methods
    Updated network request methods such that files are deleted after upload success and updated error checking methods to ensure success returned correctly.

--- RusalQRandBarcodeScanner/app/src/main/java/com/example/rusalqrandbarcodescanner/services/worker/NewItemUploadWorker.kt
+++ RusalQRandBarcodeScanner/app/src/main/java/com/example/rusalqrandbarcodescanner/services/worker/NewItemUploadWorker.kt
@@ -4,11 +4,13 @@ import android.content.Context
 import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.android.volley.ParseError
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.RequestFuture
 import com.example.rusalqrandbarcodescanner.services.FileStorage
 import com.example.rusalqrandbarcodescanner.services.HttpRequestHandler
+import org.json.JSONException
 import org.json.JSONObject
 import java.io.IOException
 import java.util.concurrent.ExecutionException
@@ -18,50 +20,74 @@ import java.util.concurrent.TimeoutException
 class NewItemUploadWorker(context : Context, workerParams : WorkerParameters) : Worker(context, workerParams) {
     override fun doWork(): Result {
 
-        val inputStream = applicationContext.openFileInput(DATA_FILE)
+        val dataFileName = inputData.getString(FileStorage.DATA_FILE_PATH)
 
         return try {
-            if (inputStream != null) {
-                val postData = FileStorage.convertToJsonObject(inputStream)
+            val inputStream = applicationContext.openFileInput(dataFileName)
 
-                val future : RequestFuture<JSONObject> = RequestFuture.newFuture()
+            try {
+                if (inputStream != null) {
+                    val postData = FileStorage.convertToJsonObject(inputStream)
 
-                val jsonObjectRequest = JsonObjectRequest(Request.Method.POST, WEB_API_URL, postData, future, future)
+                    val future: RequestFuture<JSONObject> = RequestFuture.newFuture()
 
-                HttpRequestHandler.requestQueue.add(jsonObjectRequest)
+                    val jsonObjectRequest = JsonObjectRequest(Request.Method.POST,
+                        WEB_API_URL,
+                        postData,
+                        future,
+                        future)
 
-                try {
-                    val response = future.get(30, TimeUnit.SECONDS)
-                    Log.e(TAG, response.toString())
-                    Result.success()
-                } catch (e : InterruptedException) {
-                    Log.d(TAG, "Error occurred while upload new item", e)
-                    Result.retry()
-                } catch (e : ExecutionException) {
-                    Log.d(TAG, "Error occurred while upload new item", e)
-                    Result.retry()
-                } catch (e : TimeoutException) {
-                    Log.d(TAG, "Error occurred while uploading new item", e)
-                    Result.retry()
+                    HttpRequestHandler.requestQueue.add(jsonObjectRequest)
+
+                    try {
+                        val response = future.get(30, TimeUnit.SECONDS)
+
+                        applicationContext.deleteFile(dataFileName)
+
+                        Result.success()
+
+                    } catch (e: InterruptedException) {
+                        Log.d(TAG, "Error occurred while upload new item", e)
+                        Result.retry()
+
+                    } catch (e: ExecutionException) {
+
+                        // If output is empty and causes a ParseError then the call was successful
+                        if (e.cause is ParseError) {
+                            applicationContext.deleteFile(dataFileName)
+                            Result.success()
+
+                        } else {
+                            Log.d(TAG, "Error occurred while upload new item", e)
+                            Result.retry()
+                        }
+
+                    } catch (e: TimeoutException) {
+                        Log.d(TAG, "Error occurred while uploading new item", e)
+                        Result.retry()
+                    }
+
+                } else {
+                    Log.e(TAG, "Error occurred uploading new item data - no valid filename")
+                    Result.failure()
                 }
 
-            } else {
-                Log.e(TAG, "Error occurred uploading new item data - no valid filename")
+            } catch (e: IOException) {
+                Log.d(TAG, "Error occurred uploading new item data", e)
                 Result.failure()
-            }
 
-        } catch (e : IOException) {
-            Log.d(TAG, "Error occurred uploading new item data", e)
+            } finally {
+                inputStream.close()
+            }
+        } catch (e : JSONException) {
+            Log.e(TAG, "Error occurred uploading new item data - JSON data could not be parsed")
             Result.failure()
-        } finally {
-            inputStream.close()
         }
     }
 
     companion object {
-        private val TAG = "NewItemUploadWorker"
-        private val DATA_FILE = "data.txt"
-        private val WEB_API_URL = "http://45.22.122.47:8081/api/rusal"
+        private const val TAG = "NewItemUploadWorker"
+        private const val WEB_API_URL = "http://45.22.122.47:8081/api/rusal"
     }
 
 }
--- RusalQRandBarcodeScanner/app/src/main/java/com/example/rusalqrandbarcodescanner/services/worker/ShipmentUploadWorker.kt
+++ RusalQRandBarcodeScanner/app/src/main/java/com/example/rusalqrandbarcodescanner/services/worker/ShipmentUploadWorker.kt
@@ -4,12 +4,15 @@ import android.content.Context
 import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.android.volley.ParseError
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.RequestFuture
 import com.example.rusalqrandbarcodescanner.services.FileStorage
 import com.example.rusalqrandbarcodescanner.services.HttpRequestHandler
+import org.json.JSONException
 import org.json.JSONObject
+import java.io.File
 import java.lang.Exception
 import java.util.concurrent.ExecutionException
 import java.util.concurrent.TimeUnit
@@ -18,47 +21,72 @@ import java.util.concurrent.TimeoutException
 class ShipmentUploadWorker(context : Context, workerParams : WorkerParameters) : Worker(context, workerParams) {
     override fun doWork(): Result {
 
-        val inputStream = applicationContext.openFileInput(DATA_FILE)
+        val dataFileName = inputData.getString(FileStorage.DATA_FILE_PATH)
 
         return try {
-            if (inputStream != null) {
+            val inputStream = applicationContext.openFileInput(dataFileName)
 
-                val postData = FileStorage.convertToJsonObject(inputStream)
+            try {
+                if (inputStream != null) {
 
-                val future : RequestFuture<JSONObject> = RequestFuture.newFuture()
+                    val postData = FileStorage.convertToJsonObject(inputStream)
 
-                val jsonObjectRequest = JsonObjectRequest(Request.Method.POST, WEB_API_URL, postData, future, future)
+                    val future: RequestFuture<JSONObject> = RequestFuture.newFuture()
 
-                HttpRequestHandler.requestQueue.add(jsonObjectRequest)
+                    val jsonObjectRequest = JsonObjectRequest(Request.Method.POST,
+                        WEB_API_URL,
+                        postData,
+                        future,
+                        future)
 
-                try {
-                    val response = future.get(30, TimeUnit.SECONDS)
-                    Log.e(TAG, response.toString())
-                    Result.success()
-                } catch (e : InterruptedException) {
-                    Log.d(TAG, "Error occurred uploading shipment data", e)
-                    Result.retry()
-                } catch (e : ExecutionException) {
-                    Log.d(TAG, "Error occurred uploading shipment data", e)
-                    Result.retry()
-                } catch (e : TimeoutException) {
-                    Log.d(TAG, "Error occurred uploading shipment data", e)
-                    Result.retry()
+                    HttpRequestHandler.requestQueue.add(jsonObjectRequest)
+
+                    try {
+                        val response = future.get(30, TimeUnit.SECONDS)
+
+                        applicationContext.deleteFile(dataFileName)
+
+                        Result.success()
+
+                    } catch (e: InterruptedException) {
+                        Log.d(TAG, "Error occurred uploading shipment data", e)
+                        Result.retry()
+
+                    } catch (e: ExecutionException) {
+
+                        // If output is empty and causes a ParseError then the call was successful
+                        if (e.cause is ParseError) {
+                            applicationContext.deleteFile(dataFileName)
+                            Result.success()
+
+                        } else {
+                            Log.d(TAG, "Error occurred uploading shipment data", e)
+                            Result.retry()
+                        }
+
+                    } catch (e: TimeoutException) {
+                        Log.d(TAG, "Error occurred uploading shipment data", e)
+                        Result.retry()
+                    }
+
+                } else {
+                    Log.e(TAG, "Error occurred uploading shipment data - no valid filename")
+                    Result.failure()
                 }
 
-            } else {
-                Log.e(TAG, "Error occurred uploading shipment data - no valid filename")
+            } catch (e: Exception) {
+                Log.d(TAG, "Error occurred uploading shipment data", e)
                 Result.failure()
             }
-        } catch (e : Exception) {
-            Log.d(TAG, "Error occurred uploading shipment data", e)
+
+        } catch (e : JSONException) {
+            Log.e(TAG, "Error occurred uploading shipment data - JSON data could not be parsed")
             Result.failure()
         }
     }
 
     companion object {
-        private val TAG = "ShipmentUploadWorker"
-        private val DATA_FILE = "data.txt"
-        private val WEB_API_URL = "http://45.22.122.47:8081/api/rusal/update/shipment"
+        private const val TAG = "ShipmentUploadWorker"
+        private const val WEB_API_URL = "http://45.22.122.47:8081/api/rusal/update/shipment"
     }
 }
