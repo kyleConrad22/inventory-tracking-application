ec15f0f946b6df4b6c1ca8daa2521c7ea24d8f1d
kyleConrad22 <89704118+kyleConrad22@users.noreply.github.com>
2021-10-21 11:32:17 -0500

    Update Reception Networking Method
    Updated reception networking method such that when an execution error occurs from parsing response from server the worker registers it as a success.
    
    Updated reception networking method such that generated file with data is deleted after upload success.
    
    Updated git-ignore with generated deplaymentTargetDropDown file

--- RusalQRandBarcodeScanner/app/src/main/java/com/example/rusalqrandbarcodescanner/services/worker/ReceptionUploadWorker.kt
+++ RusalQRandBarcodeScanner/app/src/main/java/com/example/rusalqrandbarcodescanner/services/worker/ReceptionUploadWorker.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.android.volley.ParseError
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.RequestFuture
 import com.example.rusalqrandbarcodescanner.services.FileStorage
 import com.example.rusalqrandbarcodescanner.services.HttpRequestHandler
 import org.json.JSONArray
+import java.io.FileNotFoundException
 import java.util.concurrent.ExecutionException
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeoutException
@@ -17,57 +19,70 @@ import java.util.concurrent.TimeoutException
 class ReceptionUploadWorker(context : Context, workerParams : WorkerParameters) : Worker(context, workerParams) {
     override fun doWork(): Result {
 
-        val inputStream = applicationContext.openFileInput(inputData.getString(FileStorage.DATA_FILE_PATH))
+        val dataFileName = inputData.getString(FileStorage.DATA_FILE_PATH)
 
         return try {
+            val inputStream = applicationContext.openFileInput(dataFileName)
 
-            if (inputStream != null) {
+            try {
+                if (inputStream != null) {
 
-                val postData = FileStorage.convertToJsonArray(inputStream)
+                    val postData = FileStorage.convertToJsonArray(inputStream)
 
-                val future: RequestFuture<JSONArray> = RequestFuture.newFuture()
+                    val future: RequestFuture<JSONArray> = RequestFuture.newFuture()
 
-                val jsonArrayRequest = JsonArrayRequest(Request.Method.POST,
-                    WEB_API_URL,
-                    postData,
-                    future,
-                    future)
+                    val jsonArrayRequest = JsonArrayRequest(Request.Method.POST, WEB_API_URL, postData, future, future)
 
-                HttpRequestHandler.requestQueue.add(jsonArrayRequest)
+                    HttpRequestHandler.requestQueue.add(jsonArrayRequest)
 
-                try {
-                    val response = future.get(30, TimeUnit.SECONDS)
-                    Log.e(TAG, response.toString())
-                    Result.success()
-                } catch (e: InterruptedException) {
-                    Log.d(TAG, "Error uploading reception data", e)
-                    Result.retry()
-                } catch (e: ExecutionException) {
-                    Log.d(TAG, "Error uploading reception data", e)
-                    Result.retry()
-                } catch (e: TimeoutException) {
-                    Log.d(TAG, "Error uploading reception data", e)
-                    Result.retry()
+                    try {
+                        val response = future.get(30, TimeUnit.SECONDS)
+
+                        applicationContext.deleteFile(dataFileName)
+
+                        Result.success()
+
+                    } catch (e: InterruptedException) {
+                        Log.d(TAG, "Error uploading reception data", e)
+                        Result.retry()
+
+                    } catch (e: ExecutionException) {
+
+                        // If output is empty and causes a ParseError then the call was successful
+                        if (e.cause is ParseError) {
+                            applicationContext.deleteFile(dataFileName)
+                            Result.success()
+                        } else {
+                            Log.d(TAG, "Error uploading reception data", e)
+                            Result.retry()
+                        }
+
+                    } catch (e: TimeoutException) {
+                        Log.d(TAG, "Error uploading reception data", e)
+                        Result.retry()
+                    }
+
+                } else {
+                    Log.e(TAG, "Error uploading reception data - no valid filename")
+                    Result.failure()
                 }
 
-            } else {
-                Log.e(TAG, "Error uploading reception data - no valid filename")
+            } catch (e: Exception) {
+                Log.d(TAG, "Error uploading reception data", e)
                 Result.failure()
+
+            } finally {
+                inputStream.close()
             }
 
-            Result.success()
-
-        } catch (e : Exception) {
-            Log.d(TAG, "Error uploading reception data", e)
+        } catch (e : FileNotFoundException) {
+            Log.e(TAG, "Error uploading reception data - data file could not be found", e)
             Result.failure()
-        } finally {
-            inputStream.close()
         }
     }
 
     companion object {
         private const val TAG = "ReceptionUploadWorker"
-        private const val DATA_FILE = "data.txt"
         private const val WEB_API_URL: String = "http://45.22.122.47:8081/api/rusal/update/reception"
     }
 }
