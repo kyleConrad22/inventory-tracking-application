f4fa816c52e8f9afde10900503acb052f5d5de28
kyleConrad22 <89704118+kyleConrad22@users.noreply.github.com>
2021-10-22 07:52:04 -0500

    Create Outline of Class to Populate Repository With Test Data

---
+++ Nasco-Database-Web-App/src/test/java/com/NascoDatabaseWebApp/Nasco/Database/Web/App/RepositoryTestData.java
@@ -0,0 +1,36 @@
+package com.NascoDatabaseWebApp.Nasco.Database.Web.App;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RepositoryTestData {
+    private List<String> HEATS = Arrays.asList(
+            "17201601", "17201604", "17201613",
+            "", "", ""
+    );
+    private List<String> LOTS = Arrays.asList(
+            "N/A", "N/A", "N/A",
+            "", "", ""
+    );
+    private List<String> BLS = Arrays.asList(
+            "SPNL06A21019", "SPNL06A21019", "SPNL06A21019",
+            "", "", ""
+    );
+    private List<String> BARCODE = Arrays.asList(
+            "", "", "",
+            "", "", ""
+    );
+    private List<String> DIMENSIONS = Arrays.asList(
+            "203X6000", "203X6000", "203X6000",
+            "", "", ""
+    );
+    private List<String> GRADE = Arrays.asList(
+            "6063 BILLETS", "6063 BILLETS", "6063 BILLETS",
+            "", "", ""
+    );
+    private List<String> QUANTITIES = Arrays.asList(
+            "", "", "",
+            "", "", ""
+    );
+
+}
