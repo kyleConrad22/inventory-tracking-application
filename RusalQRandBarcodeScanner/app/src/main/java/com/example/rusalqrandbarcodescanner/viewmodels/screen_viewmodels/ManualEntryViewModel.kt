979aa4e8a3c6d5bf6bd69141cee58d40fd14592f
kyleConrad22 <89704118+kyleConrad22@users.noreply.github.com>
2021-11-06 17:08:24 -0500

    Add Unit Test Coverage for ManualEntryViewModel

--- RusalQRandBarcodeScanner/app/src/main/java/com/example/rusalqrandbarcodescanner/viewmodels/screen_viewmodels/ManualEntryViewModel.kt
+++ RusalQRandBarcodeScanner/app/src/main/java/com/example/rusalqrandbarcodescanner/viewmodels/screen_viewmodels/ManualEntryViewModel.kt
@@ -3,21 +3,19 @@ package com.example.rusalqrandbarcodescanner.viewmodels.screen_viewmodels
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.*
 import com.example.rusalqrandbarcodescanner.viewmodels.MainActivityViewModel
-import kotlinx.coroutines.*
 import java.lang.IllegalArgumentException
 
-@DelicateCoroutinesApi
 class ManualEntryViewModel(private val mainActivityVM : MainActivityViewModel) : ViewModel() {
 
     val displaySearchButton = mutableStateOf(false)
 
     // Used to manually test logic for setSearchVis as heat number is typed
-    fun refresh() {
+    internal fun refresh() {
         setSearchButtonVis()
     }
 
     // Sets the visibility of search button
-    private fun setSearchButtonVis() {
+    internal fun setSearchButtonVis() {
         displaySearchButton.value = mainActivityVM.heatNum.value.length > 5
     }
 
--- RusalQRandBarcodeScanner/app/src/test/java/com/example/rusalqrandbarcodescanner/viewmodels_test/screen_viewmodels_test/ManualEntryViewModelTest.kt
+++ RusalQRandBarcodeScanner/app/src/test/java/com/example/rusalqrandbarcodescanner/viewmodels_test/screen_viewmodels_test/ManualEntryViewModelTest.kt
@@ -1,4 +1,76 @@
 package com.example.rusalqrandbarcodescanner.viewmodels_test.screen_viewmodels_test
 
+import androidx.compose.runtime.mutableStateOf
+import com.example.rusalqrandbarcodescanner.viewmodels.MainActivityViewModel
+import com.example.rusalqrandbarcodescanner.viewmodels.screen_viewmodels.ManualEntryViewModel
+import org.junit.Assert.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.doNothing
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.verify
+
+@ExtendWith(MockitoExtension::class)
 class ManualEntryViewModelTest {
+
+    private lateinit var viewModel : ManualEntryViewModel
+
+    @Mock
+    private lateinit var mainActivityVM : MainActivityViewModel
+
+    @BeforeEach
+    fun setup() {
+        viewModel = ManualEntryViewModel(mainActivityVM)
+    }
+
+    @Nested
+    inner class RefreshTest {
+
+        @Test
+        fun `calls setDisplayButtonVis when called`() {
+            val viewModelSpy = Mockito.spy(viewModel)
+            doNothing().`when`(viewModelSpy).setSearchButtonVis()
+
+            viewModelSpy.refresh()
+            verify(viewModelSpy).setSearchButtonVis()
+        }
+    }
+
+    @Nested
+    inner class SetDisplayButtonVisTest {
+
+        @Test
+        fun `set to false when input length is less than 6`() {
+            doReturn(mutableStateOf("12345")).`when`(mainActivityVM).heatNum
+
+            assertFalse(viewModel.displaySearchButton.value)
+            viewModel.setSearchButtonVis()
+            assertFalse(viewModel.displaySearchButton.value)
+        }
+
+        @Test
+        fun `set to true when input length is equal to 6`() {
+            doReturn(mutableStateOf("123456")).`when`(mainActivityVM).heatNum
+
+            assertFalse((viewModel.displaySearchButton.value))
+            viewModel.setSearchButtonVis()
+            assertTrue(viewModel.displaySearchButton.value)
+        }
+
+        @Test
+        fun `set to true when input length is greater than 6`() {
+            doReturn(mutableStateOf("1234567")).`when`(mainActivityVM).heatNum
+
+            assertFalse(viewModel.displaySearchButton.value)
+            viewModel.setSearchButtonVis()
+            assertTrue(viewModel.displaySearchButton.value)
+        }
+    }
+
+
 }
